41fbd7affda8c5bad27e4dfb58b24d5c26829e7c
Sam Cranford <sam@joe>
2012-09-30 15:47:12 +0200

    added fizzbuzz base files

---
+++ fizzbuzz/src/fizzbuzz.py
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class FizzBuzz(object):
+
+    def __init__(self):
+        pass
---
+++ fizzbuzz/tests/test_fizzbuzz.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import sys
+
+sys.path.append('../src')
+from fizzbuzz import FizzBuzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_bidon(self):
+        pass
+
+if __name__ == '__main__':
+    status = unittest.main()
+    sys.exit(status)

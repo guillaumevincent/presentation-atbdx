4228a484b9dab001bfdaf6e8bcf3e1826500b7c6
Sam Cranford <sam@joe>
2012-10-02 12:01:07 +0200

    fichiers de base, etat initial - pas d'implementation

---
+++ fizzbuzz/run_tests.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import glob
+import os
+import sys
+import unittest
+
+test_file_paths = glob.glob('test_*.py')
+test_names = [path[:-3] for path in test_file_paths]
+test_suites = [unittest.defaultTestLoader.loadTestsFromName(name) for name in test_names]
+suite = unittest.TestSuite(test_suites)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
---
+++ fizzbuzz/test_fizzbuzz.py
@@ -0,0 +1,12 @@
+﻿import unittest
+
+
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def testJeSuis(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
+

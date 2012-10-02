ec06fd24a39e861717e3805aaa3a36d99ca35fa0
Sam Cranford <sam@samcranford.com>
2012-10-02 14:39:29 +0200

    fichiers de base, etat initial - pas d'implementation

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -2,18 +2,8 @@
 from fizzbuzz import FizzBuzz
 
 class TestFizzBuzz(unittest.TestCase):
-
-    def setUp(self):
-        self.fizzBuzz = FizzBuzz()
-
-    def testFizzBuzzPeutDireUn(self):
-        self.assertEquals(1, self.fizzBuzz.dit('1'))
-
-    def testFizzBuzzPeutDireFizz(self):
-        self.assertEquals('Fizz', self.fizzBuzz.dit('3'))
-    
-    def testFizzBuzzPeutDireBuzz(self):
-        self.assertEquals('Buzz', self.fizzBuzz.dit('5'))
+    def testJeVis(self):
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
